1aa3497707ba07ee8fc1c6259bd835edd4fb3daf
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-17 10:53:08 +0900

    MedianOfThree?AllCaseTest.cpp 修正

--- MedianOfThree_AllCaseTest/MedianOfThree_AllCaseTest.cpp
+++ MedianOfThree_AllCaseTest/MedianOfThree_AllCaseTest.cpp
@@ -7,24 +7,52 @@ int MedianOfThree(int a, int b, int c);
 
 int main()
 {
-	int value[] = { 1,2,3 };
+	int value[][3] = {
+		{3,2,1}, // [A] a>b>c
+		{3,2,2}, // [B] a>b=c
+		{3,1,2}, // [C] a>c>b
+		{3,2,3}, // [D] a=c>b
+		{2,1,3}, // [E] c>a>b
+		{3,3,2}, // [F] a=b>c
+		{3,3,3}, // [G] a=b=c
+		{2,2,3}, // [H] c>a=b
+		{2,3,1}, // [I] b>a>c
+		{2,3,2}, // [J] b>a=c
+		{1,3,2}, // [K] b>c>a
+		{2,3,3}, // [L] b=c>a
+		{1,2,3}, // [M] c>b>a
+	};
 	int valueSize = sizeof(value) / sizeof(value[0]);
-	int i, j, k;
-	int a, b, c;
-	for (i = 0; i < valueSize; i++) {
-		a = value[i];
-		for (j = 0; j < valueSize; j++) {
-			if (j == i) continue;
-			b = value[j];
-			for (k = 0; k < valueSize; k++) {
-				if (k == i) continue;
-				if (k == j) continue;
-				c = value[k];
-				int ans = MaxOfThree(a, b, c);
-				printf("MaxOfThree(%d, %d, %d) => %d\n", a, b, c, ans);
-			}
-		}
+
+	for (int i = 0; i < valueSize; i++) {
+		int a = value[i][0];
+		int b = value[i][1];
+		int c = value[i][2];
+		int ans = MedianOfThree(a, b, c);
+		printf("MedianOfThree(%d,%d,%d) => %d\n", a, b, c, ans);
 	}
 	return 0;
 }
-
+int MedianOfThree(int a, int b, int c)
+{
+	if (a >= b) {
+		if (b >= c) {
+			return b; //【A】【B】【F】【G】
+		}
+		else if (a <= c) {
+			return a; //【D】【E】【H】
+		}
+		else {
+			return c; //【C】
+		}
+	}
+	else if (a > c) {
+		return a; //【I】
+	}
+	else if (b > c) {
+		return c; //【J】【K】
+	}
+	else {
+		return b; //【L】【M】
+	}
+}
