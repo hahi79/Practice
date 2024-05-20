2525e498556aa02bd8edbacdbd473646403e9a4a
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-20 15:55:11 +0900

    ArraySampleプロジェクトを登録

---
+++ ArraySample/ArraySample.cpp
@@ -0,0 +1,25 @@
+﻿//==========================================================
+//	配列サンプル
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+// 関数プロトタイプ
+int MaxOfFour(int a, int b, int c, int d);
+
+const int N = 5;
+
+int main()
+{
+	srand(time(NULL));
+
+	int a[N];
+	for (int i = 0; i < N; i++) {
+		a[i] = rand() % 100;
+	}
+	for (int i = 0; i < N; i++) {
+		printf("a[%d] =%d\n", i, a[i]);
+	}
+	return 0;
+}
