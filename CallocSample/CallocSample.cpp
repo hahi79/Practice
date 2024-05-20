571a32a25f4afd02e7847fd52a0071935a419d92
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-20 16:54:57 +0900

    CallocSampleプロジェクトを登録

---
+++ CallocSample/CallocSample.cpp
@@ -0,0 +1,20 @@
+﻿//==========================================================
+//	callocサンプル 
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+#include <stdlib.h> // callc(),free()
+
+int main()
+{
+	int* x = (int *)calloc(1, sizeof(int));
+	if (x == nullptr) {
+		puts("記憶域の確保に失敗しました\n");
+	}
+	else {
+		*x = 57;
+		printf("*x=%d\n", *x);
+		free(x);
+	}
+	return 0;
+}
