0f63400aa3520cd8a6a8a662fe6f5f53811b7716
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-22 11:23:05 +0900

    PrimeNumber_Improvement2プロジェクトを登録

---
+++ PrimeNumber_Improvement2/PrimeNumber_Improvement2.cpp
@@ -0,0 +1,35 @@
+﻿//==========================================================
+//	素数　改良版2
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+
+int main()
+{
+    const int N = 1000;
+    unsigned long counter = 0;
+    int prime[N/2];
+    int primePtr = 0;
+
+    prime[primePtr++] = 2;
+    prime[primePtr++] = 3;
+    for (int n = 5; n <= N; n += 2) {
+        int i;
+        bool isPrimeNumber = true;
+        for (i = 1; counter++, prime[i] * prime[i] <= n; i++) {
+            counter++;
+            if (n % prime[i] == 0) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+        if (isPrimeNumber) {
+            prime[primePtr++] = n;
+        }
+    }
+    for (int i = 0; i < primePtr; i++) {
+        printf("%d ", prime[i]);
+    }
+    printf("\n乗除算を行った回数:%lu\n", counter);
+    return 0;
+}
