cae430a4aefd9193fe7251b098ad9df496b4f6ba
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-22 10:40:47 +0900

    PrimeNumver?Improvementプロジェクトを登録

---
+++ PrimeNumber_Improvement/PrimeNumber_Improvement.cpp
@@ -0,0 +1,33 @@
+﻿//==========================================================
+//	素数　改良版
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+
+int main()
+{
+    const int N = 1000;
+    unsigned long counter = 0;
+    int prime[500];
+    int primePtr = 0;
+
+    prime[primePtr++] = 2;
+    for (int n = 3; n <= N; n+=2) {
+        int i;
+        for (i = 0; i < primePtr; i++) {
+            counter++;
+            if (n % prime[i] == 0) {
+                break;
+            }
+       
+        }
+        if (i==primePtr) {
+            prime[primePtr++] = n;
+        }
+    }
+    for (int i = 0; i < primePtr; i++) {
+        printf("%d ", prime[i]);
+    }
+    printf("\n除算を行った回数:%lu\n", counter);
+    return 0;
+}
