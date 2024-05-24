dcd0870cad8bee5026659f6b070e0553b1d2300e
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-24 15:24:56 +0900

    BinarySearch_Topプロジェクトの登録

---
+++ BinarySearch_Top/BinarySearch_Top.cpp
@@ -0,0 +1,78 @@
+﻿//==========================================================
+//	２分探索 
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> // printf()
+#include <stdlib.h> // srand(),rand()
+#include <time.h>
+// 関数プロトタイプ
+int BinarySearch(int data[], int dataSize, int ley);
+int getRandRange(int min, int max);
+
+#define SEARCH_FAIL (-1)
+
+int main()
+{
+    srand(time(NULL));
+    int dataSize;
+
+    while (true) {
+        printf("要素数:");
+        do {
+            scanf("%d", &dataSize);
+        } while (dataSize <= 0);
+
+        int* data = (int*)calloc(dataSize, sizeof(int));
+        if (data == nullptr) {
+            printf("calloc()失敗");
+            exit(1);
+        }
+        int tmp = 0;
+        for (int i = 0; i < dataSize; i++) {
+            tmp += getRandRange(0, 2);
+            data[i] = tmp;
+            printf("data[%d]:%d\n", i, data[i]);
+        }
+        int key = getRandRange(1, tmp);
+        printf("探す値:%d\n", key);
+
+        int idx = BinarySearch(data, dataSize, key);
+        if (idx == SEARCH_FAIL) {
+            puts("探索に失敗しました");
+        }
+        else {
+            printf("%d は data[%d] にあります\n", key, idx);
+        }
+        free(data);
+    }
+    return 0;
+}
+
+int BinarySearch(int data[], int dataSize, int key)
+{
+    int left = 0;
+    int right = dataSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (key == data[mid]) {
+            while (mid > 0 && data[mid - 1] == key) {
+            //    printf("mid: %d\n", mid);
+                mid--;
+            }
+            return mid;
+        }
+        else if (key < data[mid]) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return SEARCH_FAIL;
+}
+
+int getRandRange(int min, int max)
+{
+    int wid = max - min + 1;
+    return min + rand() % wid;
+}
