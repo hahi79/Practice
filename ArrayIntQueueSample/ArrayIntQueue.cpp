b65b39de8d12a0170484e8f691beb06399170d0a
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-29 13:59:39 +0900

    ArrayIntQueueSampleプロジェクトを登録

---
+++ ArrayIntQueueSample/ArrayIntQueue.cpp
@@ -0,0 +1,99 @@
+﻿//=========================================================
+//	配列Intキュー
+//==========================================================
+#include "ArrayIntQueue.h"
+#include <stdio.h> // printf()
+#include <stdlib.h> // calloc(),free()
+
+
+IntQueueResult Initialize(ArrayIntQueue* q, int max)
+{
+	q->num =  0;
+	q->que = (int*)calloc(max, sizeof(int));
+	if (q->que == nullptr) {
+		q->max = 0;
+		return INITIALIZE_FAIL;
+	}
+	q->max = max;
+	return SUCCESS;
+}
+
+IntQueueResult Enque(ArrayIntQueue* q, int x)
+{
+	if (q->num >= q->max) {
+		return ENQUE_NO_SPACE;
+	}
+	else {
+		q->que[q->num] = x;
+		q->num++;
+		return SUCCESS;
+	}
+}
+IntQueueResult Deque(ArrayIntQueue* q, int* x)
+{
+	if (q->num <= 0) {
+		return	DEQUE_NO_DATA;
+	}
+	else {
+		*x = q->que[0];
+		for (int i = 0; i < q->num - 1; i++) {
+			q->que[i] = q->que[i + 1];
+		}
+		q->num--;
+		return SUCCESS;
+	}
+}
+IntQueueResult Peek(const ArrayIntQueue* q, int* x)
+{
+	if (q->num <= 0) {
+		return	PEEK_NO_DATA;
+	}
+	else {
+		*x = q->que[0];
+		return SUCCESS;
+	}
+}
+void Clear(ArrayIntQueue* q)
+{
+	q->num = 0;
+}
+int Capacity(const ArrayIntQueue* q)
+{
+	return q->max;
+}
+int Size(const ArrayIntQueue* q)
+{
+	return q->num;
+}
+bool IsEmpty(const ArrayIntQueue* q)
+{
+	return q->num <= 0;
+}
+bool IsFull(const ArrayIntQueue* q)
+{
+	return q->num == q->max;
+}
+int Search(const ArrayIntQueue* q,int x)
+{
+	for (int i = 0; i < q->num; i++) {
+		if (q->que[i] == x) {
+			return i;
+		}
+	}
+	return SEARCH_NOT_FOUND; // -1
+}
+void Print(const ArrayIntQueue* q)
+{
+	for (int i = 0; i < q->num; i++) {
+		printf("%d ", q->que[i]);
+	}
+	putchar('\n');
+}
+void Terminate(ArrayIntQueue* q)
+{
+	if (q->que != nullptr) {
+		free(q->que);
+		q->que = nullptr;
+	}
+	q->max = q->num = 0;
+}
---
+++ ArrayIntQueueSample/ArrayIntQueue.h
@@ -0,0 +1,50 @@
+﻿//=========================================================
+//	配列Intキュー
+//==========================================================
+#ifndef __IntQueue
+#define __IntQueue
+
+typedef struct {
+	int max;  // キューの容量
+	int num;  // 現在のデータ数
+	int* que;  // キュー本体
+} ArrayIntQueue;
+
+typedef enum {
+	SUCCESS=0,
+	INITIALIZE_FAIL=1,
+	ENQUE_NO_SPACE=2,
+	DEQUE_NO_DATA=3,
+	PEEK_NO_DATA=4,
+} IntQueueResult;
+
+#define SEARCH_NOT_FOUND (-1)
+
+// キューの初期化
+IntQueueResult Initialize(ArrayIntQueue* q, int max);
+// キューにデータをエンキューする
+IntQueueResult Enque(ArrayIntQueue* q, int x);
+// キューからデータをデキューする
+IntQueueResult Deque(ArrayIntQueue* q, int* x);
+// キューからデータをピークする
+IntQueueResult Peek(const ArrayIntQueue* q, int* x);
+// 全データを削除
+void Clear(ArrayIntQueue* q);
+// キューの容量
+int Capacity(const ArrayIntQueue* q);
+// キュー上のデータ数
+int Size(const ArrayIntQueue* q);
+// キューは空か?
+bool IsEmpty(const ArrayIntQueue* q);
+// キューは満杯か?
+bool IsFull(const ArrayIntQueue* q);
+// キューからデータを探索する
+int Search(const ArrayIntQueue* q, int x);
+// 全データの表示
+void Print(const ArrayIntQueue* q);
+// キューの後始末
+void Terminate(ArrayIntQueue* q);
+
+
+
+#endif // __IntQueue
