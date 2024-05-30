1a0765367cec046f3f4aed5d4a7b33124bf92baf
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-05-30 11:15:57 +0900

    EuclideanAkgirithm?NotRecursiveプロジェクトの登録

---
+++ EuclideanAlgorithm_NotRecursive/EuclideanAlgorithm_NotRecursive.cpp
@@ -0,0 +1,37 @@
+﻿//=========================================================
+//	ユークリッドの互除法(2数の最大公約数を求める)
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+// 関数プロトタイプ
+int gcd(int x, int y); // Greatest Common Divisor を求める
+
+int main()
+{
+    while (true) {
+        int x, y;
+        do {
+            printf("１つ目の整数:");
+            scanf("%d", &x);
+        } while (x <= 0);
+        do {
+            printf("２つ目の整数:");
+            scanf("%d", &y);
+        } while (y <= 0);
+
+        int ans = gcd(x, y);
+        printf("%d と %d の最大公約数は %d\n", x, y, ans);
+    }
+    return 0;
+}
+
+int gcd(int x, int y)
+{
+    while (y != 0) {
+        int next_x = y;
+        int next_y = x % y;
+        x = next_x;
+        y = next_y;
+    }
+    return x;
+}
