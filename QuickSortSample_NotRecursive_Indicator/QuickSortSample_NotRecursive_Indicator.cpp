47996fa26a16d5d38ebe40aacd1376ed195e4f8f
浩 長谷川 <hasegawa@alfasystem.co.jp>
2024-06-04 16:12:58 +0900

    quickSortSample_NotRecursive_Indicatorプロジェクトを登録

---
+++ QuickSortSample_NotRecursive_Indicator/QuickSortSample_NotRecursive_Indicator.cpp
@@ -0,0 +1,108 @@
+﻿//=========================================================
+//	クイックソート　非再帰
+//==========================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>   // printf(),scanf()
+#include <stdlib.h>  // srand(),rand()
+#include <time.h>    // time()
+#include "IntStack.h"
+// 関数プロトタイプ
+void QuickSort(int array[], int left, int right);
+void IndicatorPush(IntStack *stk, int left, int right);
+void IndicatorPop(IntStack* stk, int left, int right);
+void dumpArray(const int array[], int arraySize);
+int getRandRange(int min, int max);
+// 交換マクロ
+#define swap(type,a,b)   do{ type tmp=a; a=b; b=tmp; }while(false)
+
+int main()
+{
+	srand(time(NULL));
+	int arraySize;
+	int* array;
+
+	while (true) {
+		do {
+			printf("要素数:");
+			scanf("%d", &arraySize);
+		} while (arraySize <= 2);
+
+		array = (int*)calloc(arraySize, sizeof(int));
+		if (array == nullptr) {
+			printf("calloc()失敗\n");
+			exit(1);
+		}
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = getRandRange(1, 100);
+		}
+		dumpArray(array, arraySize);
+		QuickSort(array,0, arraySize-1);
+		printf("昇順にソートしました\n");
+		dumpArray(array, arraySize);
+
+		free(array);
+	}
+	return 0;
+}
+
+void QuickSort(int array[], int left,int right)
+{
+	IntStack stk;
+	Initialize(&stk, right - left + 1);
+
+	Push(&stk, left);
+	Push(&stk, right);
+	IndicatorPush(&stk, left, right);
+	while (IsEmpty(&stk) == false) {
+		Pop(&stk, &right);
+		Pop(&stk, &left);
+		IndicatorPop(&stk, left, right);
+		int pl = left;
+		int pr = right;
+		int x = array[(left + right) / 2];
+		do {
+			while (array[pl] < x) pl++;
+			while (array[pr] > x) pr--;
+			if (pl <= pr) {
+				swap(int, array[pl], array[pr]);
+				pl++;
+				pr--;
+			}
+		} while (pl <= pr);
+		if (left < pr) {
+			Push(&stk, left);
+			Push(&stk, pr);
+			IndicatorPush(&stk, left, pr);
+		}
+		if (pl < right) {
+			Push(&stk, pl);
+			Push(&stk, right);
+			IndicatorPush(&stk, pl, right);
+		}
+	}
+	Terminate(&stk);
+}
+
+void IndicatorPush(IntStack* stk, int left, int right)
+{
+	printf("array[%d]～array[%d]を分割する問題をPush\n", left, right);
+	printf("stack:"); Print(stk);
+}
+void IndicatorPop(IntStack* stk, int left, int right)
+{
+	printf("array[%d]～array[%d]を分割する問題をPop\n", left, right);
+	printf("stack:"); Print(stk);
+}
+
+void dumpArray(const int array[], int arraySize)
+{
+	for (int i = 0; i < arraySize; i++) {
+		printf("array[%d] = %d\n", i, array[i]);
+	}
+}
+
+int getRandRange(int min, int max)
+{
+	int wid = max - min + 1;
+	return min + rand() % wid;
+}
